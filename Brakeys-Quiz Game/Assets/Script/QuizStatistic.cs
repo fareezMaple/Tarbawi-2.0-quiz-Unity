c580602430d4be3e9167cdc5a14c9354a94ef1ad
iqFareez <60868965+fareezMaple@users.noreply.github.com>
2020-06-05 16:10:11 +0800

    Statistic Script
    remove Unity logo splash screen

---
+++ Brakeys-Quiz Game/Assets/Script/QuizStatistic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizStatistic : MonoBehaviour
+{
+    //playerPref use
+    public string TOTAL_CORRECT_ANSWER = "TotCorAns";
+    public string TOTAL_WRONG_ANSWER = "TotWorAns";
+
+    private int totalCorrectCount;
+    private int totalWrongCount;
+
+    public void setTotalCorect(int _totalCorrectCount)
+    {
+        totalCorrectCount += _totalCorrectCount;
+    }
+    
+    public void setWrongCorect(int _totalWrongCount)
+    {
+        totalWrongCount += _totalWrongCount;
+    }
+
+    public void showQuizStat()
+    {
+        int totalALlAnswerCount = totalCorrectCount + totalWrongCount;
+        Debug.Log("Correct ans is: " + totalCorrectCount + ", wrong answer is: " + 
+                  totalWrongCount + ", total is: " + totalALlAnswerCount);
+        
+        
+    }
+}
