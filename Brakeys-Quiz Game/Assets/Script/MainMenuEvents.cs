071f788d06115accbceffbc07d09927a4c62e86b
iqFareez <60868965+fareezMaple@users.noreply.github.com>
2020-07-20 07:53:34 +0800

    added GooglePlayGames
    wow

---
+++ Brakeys-Quiz Game/Assets/Script/MainMenuEvents.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuEvents : MonoBehaviour
+{
+    private Text signInButtonText;
+    private Text authText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        signInButtonText = GameObject.Find("SignInButton").GetComponentInChildren<Text>();
+        authText = GameObject.Find("authStatus").GetComponent<Text>();
+        
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+
+        PlayGamesPlatform.DebugLogEnabled = true;
+        
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.Activate();
+        
+        PlayGamesPlatform.Instance.Authenticate(SignInCallback, true);
+        
+    }
+
+    public void SignInCallback(bool success)
+    {
+        if (success)
+        {
+            Debug.Log("Signed in!");
+            signInButtonText.text = "Sign out";
+
+            authText.text = "Signed in as " + Social.localUser.userName;
+        }
+        else
+        {
+            Debug.Log("Sign in failed");
+            signInButtonText.text = "Sign in";
+            authText.text = "Login failed!";
+        }
+    }
+
+    public void SignIn()
+    {
+        Debug.Log("clicked!!");
+        if (!PlayGamesPlatform.Instance.localUser.authenticated)
+        {
+            PlayGamesPlatform.Instance.Authenticate(SignInCallback, false);
+        }
+        else
+        {
+            PlayGamesPlatform.Instance.SignOut();
+            
+            //RESET UI
+            Debug.Log("UI resetted");
+            signInButtonText.text = "Sign in";
+        }
+    }
+}
